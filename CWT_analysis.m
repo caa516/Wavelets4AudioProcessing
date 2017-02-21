a38d7bb8a6440c9f1aff4cf42f49d050bf8bf95e
caa516 <caa516@york.ac.uk>
2017-02-21 14:36:03 +0000

    "Automated downsampling; waveform, spectrogram and CWT plots"

--- CWT_analysis.m
+++ CWT_analysis.m
@@ -1,6 +1,30 @@
 %% READ AUDIO FILE
-[audio, Fs] = audioread('/Users/CarlosArchila/Documents/University/UoY/Year4/MEng_Project/Samples/SopSax.nonvib.ff.stereo/SopSax.nonvib.ff.A4.stereo.aif');
+[audio, Fs] = audioread('/Users/CarlosArchila/Documents/University/UoY/Year4/MEng_Project/Samples/SopSax.nonvib.ff.stereo/SopSax.nonvib.ff.A5.stereo.aif');
 
-%Convert stereo to mono 
+%Convert stereo to mono
+[noRows, noColumns] = size(audio);
+if noRows>1 && noColumns>1
+    disp('Input downsampled from Stereo to Mono')
+    audio = (audio(:,1)+audio(:,2))/2;
+end
 
-audio = (audio(:,1)+audio(:,2))/2;
+%% CWT 
+n = (1:length(audio))./Fs;
+
+close all;
+figure
+% subplot(3,1,1);
+plot(n,audio);
+xlabel('Time (secs)');
+ylabel('Amplitude');
+
+
+figure
+% subplot(3,1,2)
+spectrogram(audio,128,64,128,Fs,'yaxis');
+colormap jet
+
+figure
+% subplot(3,1,3);
+cwt(audio,Fs);
+colormap jet
