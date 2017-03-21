dc4ef30ddf0a67fa59cd933d324803652e6cfabd
caa516 <caa516@york.ac.uk>
2017-03-21 14:31:48 +0000

    "Create a cell array with the audio files read from the filepaths cell array"

--- readAudioDirectory.m
+++ readAudioDirectory.m
@@ -1,4 +1,4 @@
-function [ filepaths ] = readAudioDirectory( directoryPath )
+function [ filepaths, Audio ] = readAudioDirectory( directoryPath )
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
     
@@ -7,9 +7,10 @@ function [ filepaths ] = readAudioDirectory( directoryPath )
     folder = extractfield(files, 'folder')';
     name = extractfield(files, 'name')';
     filepaths = cell(length(files),1);
+    Audio = cell(length(files),1);
     for i=1:length(files)
         filepaths(i) = strcat(folder(i),{'/'},name(i));
+        Audio{i} = readAudioFile(filepaths{i})';
     end
-
 end
 
